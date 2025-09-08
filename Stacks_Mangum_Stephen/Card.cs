1d24781060bbc682bb93df1dc4a3ff0bcab3af13
Stephen Mangum <stephenmangum01@gmail.com>
2025-09-08 08:59:21 -0500

    Add Card class and implement stack operations in Program
    Introduced a new Card class to represent playing cards. Updated Program.cs to create a deck using a stack of Card objects, perform stack operations (push, pop), and manage a hand of cards using a list. Added console output to display the state of the deck and hand.

---
+++ Stacks_Mangum_Stephen/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_Mangum_Stephen
+{
+    internal class Card
+    {
+
+        public string Number { get; set; }
+
+        public string Suit { get; set; }
+
+        public Card(string numParam, string suitParam)
+        {
+            Number = numParam;
+            Suit = suitParam;
+        }
+
+    }
+}
